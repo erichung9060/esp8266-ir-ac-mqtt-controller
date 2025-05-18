3b115eb3f738d67e38148ccd1835ab288a7b326a
RUEI TING HUNG <rthung96@gmail.com>
2025-05-19 04:52:23 +0800

    Update ir_receiver.ino

--- ir_receiver.ino
+++ ir_receiver.ino
@@ -13,38 +13,38 @@ IRsend irsend(SEND_PIN);
 decode_results results;
 
 void setup() {
-  Serial.begin(115200);
-  irrecv.enableIRIn();
-  irsend.begin();
-  Serial.println("IR Receiver and Transmitter initialized");
+    Serial.begin(115200);
+    irrecv.enableIRIn();
+    irsend.begin();
+    Serial.println("IR Receiver and Transmitter initialized");
 }
 
 void loop() {
-  if (irrecv.decode(&results)) {
-    Serial.println("\n--- IR Code Received ---");
-    Serial.println(resultToHumanReadableBasic(&results));
-    Serial.println(resultToSourceCode(&results));
-    Serial.println();
+    if (irrecv.decode(&results)) {
+        Serial.println("\n--- IR Code Received ---");
+        Serial.println(resultToHumanReadableBasic(&results));
+        Serial.println(resultToSourceCode(&results));
+        Serial.println();
 
-    if (results.decode_type == decode_type_t::UNKNOWN && results.rawlen > 0) {
-      delay(1000);
-      Serial.println("Sending RAW data (unknown format)...");
-      irsend.sendRaw((const uint16_t*)(results.rawbuf + 1), results.rawlen - 1, 38);
-    } else {
-      // 否則就用已知格式發送
-      delay(1000);
-      Serial.println(results.decode_type);
-      Serial.println(results.value);
-      Serial.println(results.bits);
-      irsend.send(results.decode_type, results.value, results.bits);
-      // irsend.send(decode_type_t::NEC, 217256175, 32); // power
-      // irsend.send(decode_type_t::NEC, 217307175, 32); // tempdown
-      // irsend.send(decode_type_t::NEC, 217305135, 32); // tempup
-      // irsend.send(decode_type_t::NEC, 217311255, 32); // wind
-      // irsend.send(decode_type_t::NEC, 1641435311, 32); // Kolin fan
-      Serial.println("IR signal resent using known protocol.");
+        if (results.decode_type == decode_type_t::UNKNOWN && results.rawlen > 0) {
+            delay(1000);
+            Serial.println("Sending RAW data (unknown format)...");
+            irsend.sendRaw((const uint16_t*)(results.rawbuf + 1), results.rawlen - 1, 38);
+            
+        } else {
+            delay(1000);
+            Serial.println(results.decode_type);
+            Serial.println(results.value);
+            Serial.println(results.bits);
+            irsend.send(results.decode_type, results.value, results.bits);
+            // irsend.send(decode_type_t::NEC, 217256175, 32); // power
+            // irsend.send(decode_type_t::NEC, 217307175, 32); // tempdown
+            // irsend.send(decode_type_t::NEC, 217305135, 32); // tempup
+            // irsend.send(decode_type_t::NEC, 217311255, 32); // wind
+            // irsend.send(decode_type_t::NEC, 1641435311, 32); // Kolin fan
+            Serial.println("IR signal resent using known protocol.");
+            
+        }
+        irrecv.resume();
     }
-
-    irrecv.resume();
-  }
 }
